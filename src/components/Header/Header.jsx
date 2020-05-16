7b1bba1ec05e798fe7d0ee845177b7fd08423c17
40 daimon <tvwmdaimon40@gmail.com>
2020-05-16 16:24:16 -0700

    add navigation menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import headerLogo from "../../img/headerLogo.svg";
 import "./Header.scss"
+import Navigation from "../Navigation/Navigation";
+
+const handleClick = () => {
+    console.log("test");
+};
 
 const Header = () => {
     return (
@@ -9,11 +14,12 @@ const Header = () => {
                 <div className="header--logo">
                     <img src={headerLogo} alt="headerLogo" />
                 </div>
-                <div className="header--hamburger hamburger">
+                <div className="header--hamburger hamburger" onClick={handleClick}>
                     <div className="header--hamburger__cross hamburger--line"></div>
                     <div className="header--hamburger__cross hamburger--line"></div>
                     <div className="header--hamburger__cross hamburger--line"></div>
                 </div>
+                <Navigation />
             </header>
         </>
     );
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Navigation.scss"
+
+const Navigation = (props) => {
+    return (
+        <>
+            <nav className="navigation">
+                <ul className="navigation--textList">
+                    <li>About</li>
+                    <li>Works</li>
+                    <li>Skills</li>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+export default Navigation;
