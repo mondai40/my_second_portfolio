1d486d35957008ca01b29a30532d5272892f1fc0
40 daimon <tvwmdaimon40@gmail.com>
2020-06-16 11:33:18 -0700

    modify a jsx and css

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Navigation.scss"
 
-const Navigation = (props) => {
+const Navigation = () => {
     return (
         <>
             <nav className="navigation">
