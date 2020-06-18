412ed559d6e7771f85949b56e9cf1ae3526839fd
40 daimon <tvwmdaimon40@gmail.com>
2020-06-17 22:09:02 -0700

    edit about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import "./About.scss";
+import AboutHeading from "./AboutHeading.svg";
+import ShinImage2 from "../../img/ShinDaimon2.JPG"
 
 
 const About = () => {
   return(
-    <div>test</div>
+    <section id="about">
+      <div className="about--heading">
+        <img src={AboutHeading} alt="AboutHeading" />
+      </div>
+      <div className="about--image">
+        <img src={ShinImage2} alt="ShinDaimon" />
+      </div>
+      <div className="about--description">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid culpa quae ut animi! Dignissimos laboriosam libero perspiciatis voluptate, nobis sunt reprehenderit aperiam saepe dolorem sit. Blanditiis dolor non unde quisquam!</p>
+      </div>
+    </section>
   );
 };
 
