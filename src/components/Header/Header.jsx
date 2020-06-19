e24babb7f8bca0e725d25654fc4b1ac768b5f068
40 daimon <tvwmdaimon40@gmail.com>
2020-06-18 21:38:15 -0700

    add navigation component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,25 +4,26 @@ import "./Header.scss"
 import Navigation from "../Navigation/Navigation";
 
 const handleClick = () => {
-    console.log("test");
+  const lines = document.querySelectorAll(".header__hamburger-line");
+  lines.forEach((line) => {
+    line.classList.toggle("header__hamburger--cross");
+  });
 };
 
 const Header = () => {
-    return (
-        <>
-            <header className="header">
-                <div className="header--logo">
-                    <img src={headerLogo} alt="headerLogo" />
-                </div>
-                <div className="header--hamburger hamburger" onClick={handleClick}>
-                    <div className="header--hamburger__cross hamburger--line"></div>
-                    <div className="header--hamburger__cross hamburger--line"></div>
-                    <div className="header--hamburger__cross hamburger--line"></div>
-                </div>
-                <Navigation />
-            </header>
-        </>
-    );
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <img className="header__logo--img" src={headerLogo} alt="headerLogo" />
+      </div>
+      <div className="header__hamburger" onClick={handleClick}>
+        <span className="header__hamburger-line"></span>
+        <span className="header__hamburger-line"></span>
+        <span className="header__hamburger-line"></span>
+      </div>
+      <Navigation />
+    </header>
+  );
 };
 
 export default Header;
