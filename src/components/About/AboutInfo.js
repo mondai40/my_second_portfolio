e0603890ac8738ef4f8f8927c39319e6aed1f120
40 daimon <tvwmdaimon40@gmail.com>
2020-07-23 21:55:52 -0700

    create css for tablet and laptop

---
+++ src/components/About/AboutInfo.js
@@ -0,0 +1,12 @@
+import ShinImage2 from "../../img/ShinDaimon2.JPG";
+
+const AboutInfo = {
+  name: "Shin Daimon",
+  image: ShinImage2,
+  description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid culpa quae ut animi! Dignissimos laboriosam libero perspiciatis voluptate, nobis sunt reprehenderit aperiam saepe dolorem sit. Blanditiis dolor non unde quisquam!",
+  skills: ["HTML", "CSS", "JavaScript", "SCSS", "React", "Git"],
+  emailUrl: "mailto:tvwmdaimon40@gmail.com",
+  resumeUrl: ""
+};
+
+export default AboutInfo
