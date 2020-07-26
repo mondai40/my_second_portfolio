dd1c3f6a0bb7b5a4d4a790dc06a4e8e34a4bf5b0
40 daimon <tvwmdaimon40@gmail.com>
2020-07-25 17:47:12 -0700

    add hero css for laptop and delete arrow

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Hero.scss"
 import ShinImage from "../../img/ShinDaimon.png";
-import HeroHeading from "./HeroHeading.svg";
+import HeroHeading from "../../img/HeroHeading.svg";
 
 const Hero = () => {
   return (
-    <section id="hero">
+    <section id="hero" className="hero">
       <div className="hero__heading">
         <img src={HeroHeading} alt="ShinDaimonHeading" />
       </div>
       <div className="hero__image">
         <img src={ShinImage} alt="ShinDaimon" />
       </div>
-      <div className="hero--arrow"><a href="#about" className="arrow__link"></a></div>
+      {/* <div className="hero--arrow"><a href="#about" className="arrow__link"></a></div> */}
     </section>
   );
 }
