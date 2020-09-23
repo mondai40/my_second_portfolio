98cf2fea7f828788c80afbb6f06e2111435ab554
40 daimon <tvwmdaimon40@gmail.com>
2020-09-22 23:34:09 -0700

    add gsap on header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import MediaQuery from "react-responsive";
+// import { Controller, Scene } from 'react-scrollmagic';
 
 import headerLogo from "../../img/headerLogo.svg";
 import SmartNavigation from "../Navigation/SmartNavigation";
 import PCNavigation from "../Navigation/PCNavigation";
 
-
 import "./Header.scss"
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Header = () => {
   const [isShow, setShow] = useState(false);
+
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(headerRef.current, {
+      duration: 2,
+      ease: 'none',
+      y: -100,
+      scrollTrigger: {
+        endTrigger: headerRef.current,
+        start: 'top',
+        toggleActions: 'play none none reverse',
+        markers: true
+      }
+    })
+
+
+    // revealRefs.current.forEach((el, index) => {
+    //   gsap.fromTo(el, {
+    //     autoAlpha: 0
+    //   }, {
+    //     duration: 1,
+    //     autoAlpha: 1,
+    //     ease: 'none',
+    //     scrollTrigger: {
+    //       id: `section-${index + 1}`,
+    //       trigger: el,
+    //       start: 'top bottom-=30px',
+    //       toggleActions: 'play none none reverse',
+    //     }
+    //   })
+    // });
+    console.log(headerRef.current.offsetHeight);
+  }, []);
 
   const handleClick = () => {
     const lines = document.querySelectorAll(".header__hamburger-line");
@@ -20,7 +58,7 @@ const Header = () => {
   };
 
   return (
-    <header className="header">
+    <header className="header" id="header" ref={headerRef}>
       <div className="header__logo" onClick={isShow ? handleClick : function(){}}>
         <a href="#hero">
           <img className="header__logo--img" src={headerLogo} alt="headerLogo" />
